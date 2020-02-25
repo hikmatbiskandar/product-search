ce8de9dffcc0f30618d60c2eff1a9d9704f15cae
Hikmat <hikmat@cpgbl.com>
2020-02-25 17:55:03 +0800

    + Update on the UI

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -7,7 +7,8 @@
 
         <div class="pt-1">
             <el-row>
-                <el-button @click="searchOn(site)" v-for="site in sites.malaysia" v-bind:key="site.name" round>{{site.name}}</el-button>
+                <el-button :type="site.is_searching ? 'success' : ''" @click="toggleSelect(site)" v-for="site in sites.malaysia" v-bind:key="site.name" round>{{site.name}}</el-button>
+                <el-button type="success"  @click="searchOnAll()" icon="el-icon-search" round>Go</el-button>
             </el-row>
         </div>
 
@@ -28,62 +29,74 @@
                     {
                         "name": "Lazada",
                         "url": "https://www.lazada.com.my/catalog/?q={query}",
-                        "type": "all"
+                        "type": "all",
+                        "is_searching": true
                     },
                     {
                         "name": "Shopee",
                         "url": "https://shopee.com.my/search?keyword={query}",
-                        "type": "all"
+                        "type": "all",
+                        "is_searching": true
                     },
                     {
                         "name": "Lelong",
                         "url": "https://www.lelong.com.my/catalog/all/list?TheKeyword={query}",
-                        "type": "p2p"
+                        "type": "p2p",
+                        "is_searching": true
                     },
                     {
                         "name": "Zalora",
                         "url": "https://www.zalora.com.my/catalog/?q={query}",
-                        "type": "fashion"
+                        "type": "fashion",
+                        "is_searching": true
                     },
                     {
                         "name": "GoShop",
                         "url": "https://www.goshop.com.my/search?keyword={query}",
-                        "type": "tv-marketing"
+                        "type": "tv-marketing",
+                        "is_searching": true
                     },
                     {
                         "name": "eBay (MY)",
                         "url": "https://www.ebay.com.my/sch/i.html?_nkw={query}",
-                        "type": "all"
+                        "type": "all",
+                        "is_searching": true
                     },
                     {
                         "name": "Hermo",
                         "url": "https://www.hermo.my/search/product?keyword={query}",
-                        "type": "fashion"
+                        "type": "fashion",
+                        "is_searching": true
                     },
                     {
                         "name": "Qoo10",
                         "url": "https://www.qoo10.my/s/?keyword={query}",
-                        "type": "all"
+                        "type": "all",
+                        "is_searching": true
                     },
                     {
                         "name": "PrestoMall",
                         "url": "https://www.prestomall.com/totalsearch/TotalSearchAction/searchTotal.do?kwd={query}",
-                        "type": "all"
+                        "type": "all",
+                        "is_searching": true
                     },
                     {
                         "name": "Fashion Valet",
                         "url": "https://www.fashionvalet.com/catalogsearch/result/?q={query}",
-                        "type": "fashion"
+                        "type": "fashion",
+                        "is_searching": true
                     },
                     {
                         "name": "Mudah",
                         "url": "https://www.mudah.my/Malaysia/{query}-for-sale?q={query}",
-                        "type": "p2p"
+                        "type": "p2p",
+                        "is_searching": true
                     },
                     {
                         "name": "EZBuy",
                         "url": "https://ezbuy.my/category/?keyWords={query}",
-                        "type": "all"
+                        "type": "all",
+                        "is_searching": true
                     },
 
                     ]
@@ -91,8 +104,18 @@
             }
         },
         methods: {
-            searchOn(site) {
-                window.open(site.url.replace("{query}", this.keyword), "_blank");
+            toggleSelect(site) {
+                site.is_searching = !site.is_searching;
+            },
+            searchOnAll() {
+
+                if (this.keyword == "") return false;
+
+                for (var i = 0; i < this.sites.malaysia.length; i++) {
+                    var single_site = this.sites.malaysia[i];
+                    if (single_site.is_searching == true) window.open(single_site.url.replace(/{query}/g, this.keyword), "_blank");    
+                }
+                
                 focus();
             }
         } 
